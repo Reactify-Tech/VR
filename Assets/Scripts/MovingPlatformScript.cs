dc424682b6507e5a21221d6448b43ef69db9947a
Emily <emilymurray824@icloud.com>
2026-04-13 12:48:43 +0100

    FIXED moving platform
    would not move character, now does

--- Assets/Scripts/MovingPlatformScript.cs
+++ Assets/Scripts/MovingPlatformScript.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
 using Unity.XR.CoreUtils;
 
-public class MovingPlatformScript : MonoBehaviour
+public class MovingPlatformXRRide : MonoBehaviour
 {
     [SerializeField] private Transform xrOrigin;
-    [SerializeField] private Collider rideTrigger;
     [SerializeField] private bool inheritRotation = false;
 
     private bool playerOnPlatform;
@@ -37,12 +36,12 @@ public class MovingPlatformScript : MonoBehaviour
         }
 
         Vector3 deltaPos = transform.position - lastPosition;
-        Quaternion deltaRot = transform.rotation * Quaternion.Inverse(lastRotation);
-
         xrOrigin.position += deltaPos;
 
         if (inheritRotation)
         {
+            Quaternion deltaRot = transform.rotation * Quaternion.Inverse(lastRotation);
+
             Vector3 pivot = transform.position;
             Vector3 offset = xrOrigin.position - pivot;
             offset = deltaRot * offset;
@@ -54,26 +53,17 @@ public class MovingPlatformScript : MonoBehaviour
         lastRotation = transform.rotation;
     }
 
-    private bool IsPlayer(Collider other)
-    {
-        return other.GetComponentInParent<XROrigin>() != null;
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        if (IsPlayer(other))
-        {
-            playerOnPlatform = true;
-            lastPosition = transform.position;
-            lastRotation = transform.rotation;
-        }
+        Debug.Log("Entered trigger: " + other.name);
+        playerOnPlatform = true;
+        lastPosition = transform.position;
+        lastRotation = transform.rotation;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (IsPlayer(other))
-        {
-            playerOnPlatform = false;
-        }
+        Debug.Log("Exited trigger: " + other.name);
+        playerOnPlatform = false;
     }
 }
